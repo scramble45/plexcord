539b16a8d621d7c69cc42104e27df1a1a38d51fc
r0r0 <scramble@gmail.com>
2020-01-17 12:27:41 -0600

    remove log

--- routes/files.js
+++ routes/files.js
@@ -5,7 +5,6 @@ const router    = express.Router()
 
 // files by: id + filename
 router.get('/:id/:filename', function (req, res, next) {
-  console.log('got here')
   fileQuery(req, res, (err, results) => {
     if (err) debug(err)
     debug(results)
