c7b6ff7384dfc34a78c95b917f052f6f2b7a942b
r0r0 <scramble@gmail.com>
2020-01-19 07:23:17 -0600

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "root": true,
+  "extends": "eslint:recommended",
+  "rules": {
+    "semi"                    : ["error", "never"],
+    "indent"                  : ["error", 2, {"SwitchCase":1, "VariableDeclarator": { "var": 2, "let": 2, "const": 3}}],
+    "linebreak-style"         : ["error", "unix"],
+    "eqeqeq"                  : ["error", "smart"],
+    "quotes"                  : ["error", "single"],
+    "curly"                   : ["error", "multi-line"],
+    "brace-style"             : ["error", "1tbs", {"allowSingleLine": true}],
+    "camelcase"               : "error",
+    "no-trailing-spaces"      : "error",
+    "no-mixed-spaces-and-tabs": "error",
+    "no-tabs"                 : "error",
+    "no-console"              : "off",
+    "no-unused-vars"          : ["error", { "vars": "all", "args": "none" }],
+    "keyword-spacing"         : "error"
+  },
+  "env": {
+    "es6": true,
+    "browser": true,
+    "node": true,
+    "mocha": true
+  },
+  "ecmaFeatures": {},
+  "plugins": []
+}
